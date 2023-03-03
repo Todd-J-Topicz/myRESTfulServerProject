8f02a77059a8d80b5fb245d9624d11011c0880c8
Todd-J-Topicz <tjtopicz@gmail.com>
2023-03-03 14:55:29 -0600

    seed.js, migration.js, and dbConn.js files added - Additions to package.json file

---
+++ migration.js
@@ -0,0 +1,32 @@
+const { Pool } = require('pg');
+const DATABASE_URL = process.env.DATABASE_URL;
+const pool = require('./dbConn');
+
+//run migration SQL:
+pool.query(`CREATE TABLE IF NOT EXISTS studios (
+    id SERIAL PRIMARY KEY,
+	name VARCHAR (100) NOT NULL)`, (err, data) => {
+        if (err) {
+            console.log("CREATE TABLE studios FAILED")
+        } else {
+            console.log("studios CREATE TABLE successful")
+        }
+    }
+);
+
+pool.query(`CREATE TABLE IF NOT EXISTS games (
+    id SERIAL PRIMARY KEY,
+	name VARCHAR (100) NOT NULL,
+    year INT NOT NULL,
+	studio_id INT NOT NULL, 
+    FOREIGN KEY (studio_id) REFERENCES studio(id) ON DELETE CASCADE)`, (err, data) => {
+        if (err) {
+            console.log("CREATE TABLE games FAILED")
+        } else {
+            console.log("games CREATE TABLE successful")
+        }
+    }
+);
+
+//close connection:
+pool.end();
---
+++ seed.js
@@ -0,0 +1,49 @@
+const { Pool } = require('pg');
+const pool = require('./dbConn');
+
+pool.query('SELECT COUNT(*) FROM studio', (err,data) => {
+    console.log(data.rows[0]['count']);
+
+    if (data.rows[0]['count'] == 0){
+        pool.query(`INSERT INTO studio (name) VALUES 
+            ('Activision'),
+            ('Bethesda Softworks'),
+            ('Bungie'),
+            ('Massive Entertainment')`,
+            (err, data) => {
+                if (err){
+                    console.log("INSERT FAILED")
+                } else {
+                    console.log("INSERT SUCCESSFUL")
+                }
+            }
+        )
+    }
+})
+
+pool.query('SELECT COUNT(*) FROM games', (err,data) => {
+    console.log(data.rows[0]['count']);
+
+    if (data.rows[0]['count'] == 0){
+        pool.query(`INSERT INTO games (name, year, studio_id) VALUES 
+            ('Modern Warfare 2', 2022, 1),
+            ('Fallout 76', 2018, 2),
+            ('Destiny 2', 2017, 3),
+            ('Division 2', 2019, 4),
+            ('Halo: Reach', 2010, 3),
+            ('The Elder Scrolls V: Skyrim', 2011, 2)`,
+            (err, data) => {
+                if (err){
+                    console.log("INSERT FAILED")
+                } else {
+                    console.log("INSERT SUCCESSFUL")
+                }
+            }
+        )
+    }
+})
+
+
+
+//close connection:
+pool.end();
