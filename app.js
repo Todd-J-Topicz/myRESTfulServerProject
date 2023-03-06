1f9115df7c750b826a7563ced3b0b7c844d60097
Todd-J-Topicz <tjtopicz@gmail.com>
2023-03-06 17:28:24 -0600

    front end additions

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ $studioSearch.click(function(){
 
   $gameSearch.click(function(){
     console.log("game click works");
-    studioSearch();
+    gameSearch();
   });
 
 function studioSearch(){
@@ -17,6 +17,18 @@ function studioSearch(){
     $.get("https://no-hope-web-services.onrender.com/api/studio/", (data) =>{
         console.log(data);
 
+        $('#results').replaceWith('<div id="results"></div>');
+
+        for (let x = 0; x < data.length; x++){
+            let $span = $('<span class="studio-card"></span>');
+
+            let $h2 = $('<h2 class="game-name"></h2>');
+            $($h2).text(data[x].name);
+            $span.append($h2);
+
+            $span.appendTo($('#results'));
+            
+        }  
     });
 };
 
@@ -25,5 +37,26 @@ function gameSearch(){
     $.get("https://no-hope-web-services.onrender.com/api/games", (data) =>{
       console.log(data);
 
+      $('#results').replaceWith('<div id="results"></div>');
+
+        for (let x = 0; x < data.length; x++){
+            let $span = $('<span class="game-card"></span>');
+
+            let $h2 = $('<h2 class="game-name"></h2>');
+            $($h2).text(data[x].name);
+            $span.append($h2);
+
+            let $h3 = $('<h3 class="game-year"></h3');
+            $($h3).text(data[x].year);
+            $h2.append($h3);
+
+            let $h4 = $('<h4 class="game-studio"></h4>');
+            $($h4).text(data[x].studio_id);
+            $h2.append($h4);
+
+            $span.appendTo($("#results"));
+        }
+
     });
 };
+
